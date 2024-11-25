bd5c8fb8daecc926cb930e3b5d60954a44961374
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2024-11-25 08:52:14 -0300

    removed setupNodeEvents config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,27 +4,4 @@ export default defineConfig({
   screenshotOnRunFailure: false,
   video: false,
   defaultBrowser: 'chrome',
-  e2e: {
-    setupNodeEvents(on, config) {
-      const scrappedData: any[] = [] // TODO - create a type for this
-
-      on('task', {
-        setScrappedData(data) {
-          console.log(`Scrapped data: ${data}`, 'log');
-
-          if (!data) return null;
-
-          const stringifiedData = JSON.stringify(data, null, 2);
-
-          scrappedData.push(stringifiedData);
-
-          return stringifiedData;
-        },
-        getScrappedData() {
-          console.log(`Scrapped data: ${scrappedData}`, 'log');
-          return scrappedData;
-        },
-      });
-    },
-  },
 });
