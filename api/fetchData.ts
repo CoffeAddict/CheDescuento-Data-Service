842f391a0ad9e31c9cdf5fc993ed84db92d53e45
Andres Rodriguez <andresrodriguezesteban@gmail.com>
2024-11-25 22:58:30 -0300

    migrated from esbuild to tsc & tsx

--- api/fetchData.ts
+++ api/fetchData.ts
@@ -1,16 +1,13 @@
 import { logMessage } from '../utils/logMessage';
 import { scrapData } from '../services/cypressService';
 import { moveToDirectory } from '../utils/fileUtils';
-// import { exec } from 'child_process';
 
 export default async function fetchData() {
     try {
-        // exec('ls', (error, stdout, stderr) => {
-        //     console.log(`Files: \n${stdout}`);
-        // })
-        await scrapData('0.0.1-coto')
-        moveToDirectory('0.0.1-coto.json')
+        scrapData('0.0.1-coto')
+            .then(() => moveToDirectory('0.0.1-coto.json'))
     } catch (error) {
+        console.log(error)
         logMessage(error, 'error')
     }
 }
--- utils/fileUtils.ts
+++ utils/fileUtils.ts
@@ -1,27 +1,5 @@
-import { writeFile, renameSync } from 'fs';
+import fs from 'fs';
 import path from 'path';
-import { moveFile } from 'move-file';
-import { logMessage } from './logMessage';
-
-/**
- *
- * Writes JSON data to a file
- * @param {*} data - JSON data to write to file
- * @param {*} filename - Name of the file to write
- * @param {string} [directory='/public'] - Directory to write the file to
- */
-export async function writeToFile (data: any, filename: string, directory: string = '/public') {
-  try {
-    const filePath = path.join(process.cwd(), directory, `${filename}.json`);
-    const dataString = JSON.stringify(data);
-
-    writeFile(filePath, dataString, (err: Error | null) => {
-      err ? logMessage(err, 'error') : logMessage(`File written successfully on ${filePath}`);
-    });
-  } catch (error) {
-    logMessage(error, 'error');
-  }
-};
 
 /**
  * Moves a file to a specified directory.
@@ -30,12 +8,15 @@ export async function writeToFile (data: any, filename: string, directory: strin
  * @param {string} [filePath=''] - The file path to move.
  * @param {string} [directory='/public'] - The directory to move the file to.
  */
-export async function moveToDirectory (filePath: string = '', directory: string = 'public') {
-  // Get the file name from the file path
-  const fileName = filePath.replace(/.*\//, '');
+export async function moveToDirectory (fileName: string, oldPath: string = '', newPath: string = 'public') {
+  // Join the file name with the old directory path
+  const oldFilePath = path.join(oldPath, fileName);
 
-  // Join the file name with the directory path
-  const newFilePath = path.join(directory, fileName);
+  // Join the file name with the new directory path
+  const newFilePath = path.join(newPath, fileName);
 
-  await moveFile(filePath, newFilePath);
+  // Create the new directory if it doesn't exist
+  if (!fs.existsSync(newPath)) fs.mkdirSync(newPath, { recursive: true })
+
+  await fs.promises.rename(oldFilePath, newFilePath);
 }
